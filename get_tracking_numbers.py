38860b961604185b6589244d9e4b93378f2a389f
Gustav Brodman <gus.brodman@gmail.com>
2019-08-10 16:49:39 -0400

    Send emails on exceptions with the content

--- get_tracking_numbers.py
+++ get_tracking_numbers.py
@@ -1,5 +1,6 @@
 import yaml
 import sys
+import traceback
 import upload_tracking_numbers
 from email_sender import EmailSender
 from tracking_retriever import TrackingRetriever
@@ -14,6 +15,12 @@ EMAIL_CONFIG = CONFIG['email']
 
 DRIVER_CREATOR = None
 
+def send_error_email(email_sender, subject, exception):
+    type, value, trace = sys.exc_info()
+    formatted_trace = traceback.format_tb(trace)
+    lines = [str(type), str(value)] + formatted_trace
+    email_sender.send_email_content(subject, "\n".join(lines))
+
 def upload_numbers(email_sender, groups_dict):
     for group, trackings in groups_dict.items():
         numbers = [tracking.tracking_number for tracking in trackings]
@@ -22,7 +29,8 @@ def upload_numbers(email_sender, groups_dict):
             try:
                 upload_tracking_numbers.upload(numbers, group, group_config['username'], group_config['password'], DRIVER_CREATOR)
             except Exception as e:
-                email_sender.send_email_content("Error uploading tracking numbers", str(e))
+                send_error_email(email_sender, "Error uploading tracking numbers", e)
+                raise e
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -30,8 +38,13 @@ if __name__ == "__main__":
     else:
         DRIVER_CREATOR = DriverCreator("CHROME")
 
-    tracking_retriever = TrackingRetriever(CONFIG, DRIVER_CREATOR)
-    groups_dict = tracking_retriever.get_trackings()
     email_sender = EmailSender(EMAIL_CONFIG)
+    tracking_retriever = TrackingRetriever(CONFIG, DRIVER_CREATOR)
+    try:
+        groups_dict = tracking_retriever.get_trackings()
+    except Exception as e:
+        send_error_email(email_sender, "Error retrieving emails", e)
+        raise e
+
     email_sender.send_email(groups_dict)
     upload_numbers(email_sender, groups_dict)
