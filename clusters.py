b7596875d866edb96b1ddebc6c3f347f5c95bf01
Gustav Brodman <gus.brodman@gmail.com>
2019-09-01 11:07:02 -0400

    Initial draft of adding purchase orders to clusters

--- clusters.py
+++ clusters.py
@@ -8,7 +8,7 @@ CLUSTERS_FILE = OUTPUT_FOLDER + "/clusters.pickle"
 class Cluster:
 
   def __init__(self, group):
-    self._initiate(set(), set(), group, 0, 0, '0')
+    self._initiate(set(), set(), group, 0, 0, '0', set())
 
   def _initiate(self,
                 orders,
@@ -16,13 +16,15 @@ class Cluster:
                 group,
                 expected_cost,
                 tracked_cost,
-                last_ship_date='0'):
+                last_ship_date='0',
+                purchase_orders=set()):
     self.orders = orders
     self.trackings = trackings
     self.group = group
     self.expected_cost = expected_cost
     self.tracked_cost = tracked_cost
     self.last_ship_date = last_ship_date
+    self.purchase_orders = purchase_orders
 
   def __setstate__(self, state):
     self._initiate(**state)
@@ -31,6 +33,14 @@ class Cluster:
     return "orders: %s, trackings: %s, group: %s, expected cost: %s, tracked cost: %s" % (
         str(self.orders), str(self.trackings), self.group,
         str(self.expected_cost), str(self.tracked_cost))
+
+  def merge_with(self, other):
+    self.orders.update(other.orders)
+    self.trackings.update(other.trackings)
+    self.expected_cost += other.expected_cost
+    self.tracked_cost += other.tracked_cost
+    self.last_ship_date = max(self.last_ship_date, other.last_ship_date)
+    self.purchase_orders.update(other.purchase_orders)
 
 
 def dedupe_clusters(clusters):
@@ -80,3 +90,34 @@ def update_clusters(all_clusters, trackings_dict):
       cluster.orders.update(tracking.order_ids)
       cluster.trackings.add(tracking.tracking_number)
       cluster.last_ship_date = max(cluster.last_ship_date, tracking.ship_date)
+
+
+def merge_by_purchase_orders(clusters):
+  while True:
+    prev_length = len(clusters)
+    clusters = run_merge_iteration(clusters)
+    if len(clusters) == prev_length:
+      break
+  return clusters
+
+
+def run_merge_iteration(clusters):
+  result = []
+  for cluster in clusters:
+    to_merge = find_by_purchase_orders(cluster, result)
+    if to_merge:
+      to_merge.merge_with(cluster)
+    else:
+      result.append(cluster)
+  return result
+
+
+def find_by_purchase_orders(cluster, all_clusters):
+  if not cluster.purchase_orders:
+    return None
+
+  for candidate in all_clusters:
+    if candidate.purchase_orders.intersection(cluster.purchase_orders):
+      return candidate
+
+  return None
