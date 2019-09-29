5de091334fd286e876c893992ab35ca23bc76535
Gustav Brodman <gus.brodman@gmail.com>
2019-09-29 10:18:43 -0400

    Add to-email in emails

--- lib/email_sender.py
+++ lib/email_sender.py
@@ -23,8 +23,8 @@ class EmailSender:
     content = "Tracking number / order number(s) per group:\n\n"
     for group, trackings in groups_dict.items():
       numbers = [
-          tracking.tracking_number + " / " + ", ".join(tracking.order_ids)
-          for tracking in trackings
+          tracking.tracking_number + " / " + ", ".join(tracking.order_ids) +
+          " / " + tracking.to_email for tracking in trackings
       ]
       content += group
       content += '\n'
