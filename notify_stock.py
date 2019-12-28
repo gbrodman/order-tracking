5b10c45441362c4cb061b20c0898be62e8f8bc92
Gustav Brodman <gus.brodman@gmail.com>
2019-12-27 19:32:33 -0700

    Be more resilient to failures when running indef

--- notify_stock.py
+++ notify_stock.py
@@ -46,30 +46,34 @@ if __name__ == "__main__":
   objects_to_sheet = ObjectsToSheet()
 
   while True:
-    item_list = objects_to_sheet.download_from_sheet(items.from_row, sheet_id,
-                                                     "Items")
+    try:
+      item_list = objects_to_sheet.download_from_sheet(items.from_row, sheet_id,
+                                                       "Items")
+  
+      prices_map = retriever.get_prices([item.asin for item in item_list])
+      new_items = []
+      for item in item_list:
+        new_price = prices_map.get(item.asin, None)
+        prev_price = item.price
+        item.price = new_price
+        if new_price and (not prev_price or new_price < prev_price) and (
+            not item.notify_if_below or new_price < item.notify_if_below):
+          new_items.append(item)
+  
+      objects_to_sheet.upload_to_sheet(item_list, sheet_id, "Items")
+  
+      email_list = objects_to_sheet.download_from_sheet(emails.from_row, sheet_id,
+                                                        "Emails")
+      email_sender = EmailSender(config['email'])
+  
+      if new_items and email_list:
+        content = create_email_content(new_items, item_list, sheet_id)
+        recipients = [email_obj.email_address for email_obj in email_list]
+        email_sender.send_email_content("🚨 Newly in-stock Amazon items 🚨",
+                                        content, recipients)
+    except Exception as e:
+      print(str(e))
 
-    prices_map = retriever.get_prices([item.asin for item in item_list])
-    new_items = []
-    for item in item_list:
-      new_price = prices_map.get(item.asin, None)
-      prev_price = item.price
-      item.price = new_price
-      if new_price and (not prev_price or new_price < prev_price) and (
-          not item.notify_if_below or new_price < item.notify_if_below):
-        new_items.append(item)
-
-    objects_to_sheet.upload_to_sheet(item_list, sheet_id, "Items")
-
-    email_list = objects_to_sheet.download_from_sheet(emails.from_row, sheet_id,
-                                                      "Emails")
-    email_sender = EmailSender(config['email'])
-
-    if new_items and email_list:
-      content = create_email_content(new_items, item_list, sheet_id)
-      recipients = [email_obj.email_address for email_obj in email_list]
-      email_sender.send_email_content("🚨 Newly in-stock Amazon items 🚨",
-                                      content, recipients)
     if args.run_indefinitely:
       print("Sleeping for five minutes...")
       time.sleep(300)
