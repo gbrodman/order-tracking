8055f0833556cf71e676f0f46cc67fe60b46c815
kellynicole1992 <64326430+kellynicole1992@users.noreply.github.com>
2020-05-10 15:45:17 -0400

    Update email_auth.py

--- lib/email_auth.py
+++ lib/email_auth.py
@@ -11,51 +11,51 @@ from googleapiclient.discovery import build
 from lib.config import open_config
 from imaplib import IMAP4_SSL
 
-config = open_config()
-email_config = config['email']
-gmail_url = "https://mail.google.com/"
-imapUrl = "imap.gmail.com"
-smtpUrl = "smtp.gmail.com"
-smtpPort = "587"
-username= email_config["username"]
+CONFIG = open_config()
+EMAIL_CONFIG = CONFIG['email']
+GMAIL_URL = "https://mail.google.com/"
+IMAPURL = "imap.gmail.com"
+SMTPURL = "smtp.gmail.com"
+SMTPPORT = "587"
+USERNAME= EMAIL_CONFIG["username"]
 
 
 def email_authentication():
    
-  mail = IMAP4_SSL(imapUrl)
-  if "password"  in email_config and email_config["password"]:
-    mail.login(email_config['username'], email_config['password'])
+  mail = IMAP4_SSL(IMAPURL)
+  if "password"  in EMAIL_CONFIG and EMAIL_CONFIG["password"]:
+    mail.login(EMAIL_CONFIG['username'], EMAIL_CONFIG['password'])
   else:
-    creds = get_oauth_credentials(gmail_url)
+    creds = get_oauth_credentials()
     client_credentials = creds.get_access_token().access_token
-    authstring = f"user={username}\1auth=Bearer {client_credentials}\1\1"
+    authstring = f"user={USERNAME}\1auth=Bearer {client_credentials}\1\1"
     mail.authenticate('XOAUTH2', lambda x: authstring)
   return mail
 
 
 def send_email(recipients, message):
-  if "password"  in email_config and email_config["password"]:
-    s = smtplib.SMTP(smtpUrl,
-                     smtpPort)
+  if "password"  in EMAIL_CONFIG and EMAIL_CONFIG["password"]:
+    s = smtplib.SMTP(SMTPURL,
+                     SMTPPORT)
     s.starttls()
-    s.login(email_config['username'], email_config['password'])
-    s.sendmail(email_config['username'], recipients, message.as_string())
+    s.login(EMAIL_CONFIG['username'], EMAIL_CONFIG['password'])
+    s.sendmail(EMAIL_CONFIG['username'], recipients, message.as_string())
     s.quit()
   else:
-    creds = get_oauth_credentials(gmail_url)
+    creds = get_oauth_credentials()
     service = build('gmail','v1',credentials=creds)
     raw = base64.urlsafe_b64encode(message.as_bytes())
     raw = raw.decode()
     body = {'raw': raw}
     message=body
-    service.users().messages().send(userId=self.email_config['username'],body=message).execute()
+    service.users().messages().send(userId=EMAIL_CONFIG['username'],body=message).execute()
 
 
 def get_oauth_credentials():
   store = file.Storage('storage.json')
   creds = store.get()
   if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', gmail_url)
+    flow = client.flow_from_clientsecrets('client_secret.json', GMAIL_URL)
     creds = tools.run_flow(flow, store)
   else:
     creds.refresh(Http())
