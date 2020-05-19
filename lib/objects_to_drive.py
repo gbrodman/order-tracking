04cf9a8efc42d1431e04ee6bc50225b8fdd7c461
Ben McIlwain <cydeweys@gmail.com>
2020-05-19 17:29:38 -0400

    @Retry communications with Drive for less flakey processing (#159)
    * @Retry communications with Drive for less flakey processing
    
    * @Retry Sheet uploads/downloads as well

--- lib/objects_to_drive.py
+++ lib/objects_to_drive.py
@@ -1,8 +1,11 @@
-from lib import drive_service
 import io
 import pickle
-from googleapiclient.http import MediaIoBaseDownload
+
 from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseDownload
+from tenacity import retry, stop_after_attempt, wait_exponential
+
+from lib import drive_service
 
 
 class ObjectsToDrive:
@@ -10,6 +13,11 @@ class ObjectsToDrive:
   def __init__(self):
     self.service = drive_service.create_drive()
 
+
+  @retry(
+      stop=stop_after_attempt(3),
+      wait=wait_exponential(multiplier=1, min=2, max=16),
+      reraise=True)
   def save(self, config, filename, local_filename):
     folder_id = self._get_folder_id(config)
     file_metadata = {"name": filename, "mimeType": "*/*"}
@@ -26,6 +34,11 @@ class ObjectsToDrive:
       # update
       self.service.files().update(fileId=file_id, media_body=media).execute()
 
+
+  @retry(
+      stop=stop_after_attempt(3),
+      wait=wait_exponential(multiplier=1, min=2, max=16),
+      reraise=True)
   def load(self, config, filename):
     folder_id = self._get_folder_id(config)
     files_in_folder = self._get_files_in_folder(folder_id)
@@ -34,6 +47,7 @@ class ObjectsToDrive:
       return None
     return self._download_file(file_id)
 
+
   def _get_folder_id(self, config):
     if "driveFolderId" in config:
       return config["driveFolderId"]
@@ -41,16 +55,19 @@ class ObjectsToDrive:
       return config["driveFolder"]
     raise Exception("Please include 'driveFolderId' in the config")
 
+
   def _find_file_id(self, files, filename):
     for file in files:
       if file['name'] == filename:
         return file['id']
     return None
 
+
   def _get_files_in_folder(self, folder_id):
     return self.service.files().list(
         q="'%s' in parents" % folder_id,
         fields='nextPageToken, files(id, name)').execute()['files']
+
 
   def _download_file(self, file_id):
     request = self.service.files().get_media(fileId=file_id)
