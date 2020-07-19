c9e8ca4901b13d67dbe3faabfd5815eeb45a4f78
Gustav Brodman <gus.brodman@gmail.com>
2020-07-19 14:41:07 -0400

    Add a debounce annotation for order-to-Drive flushing

---
+++ lib/debounce.py
@@ -0,0 +1,22 @@
+# Taken from https://gist.github.com/walkermatt/2871026
+
+
+from threading import Timer
+
+
+def debounce(wait):
+    """ Decorator that will postpone a functions
+        execution until after wait seconds
+        have elapsed since the last time it was invoked. """
+    def decorator(fn):
+        def debounced(*args, **kwargs):
+            def call_it():
+                fn(*args, **kwargs)
+            try:
+                debounced.t.cancel()
+            except(AttributeError):
+                pass
+            debounced.t = Timer(wait, call_it)
+            debounced.t.start()
+        return debounced
+    return decorator
