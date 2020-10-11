83750590d395257b3146351d96c1f9b8bec74d18
Ben McIlwain <cydeweys@gmail.com>
2020-10-11 11:30:15 -0400

    Add retrying for Drive operations

--- lib/object_retriever.py
+++ lib/object_retriever.py
@@ -1,6 +1,7 @@
 import pickle
 import os.path
 
+from tenacity import retry, stop_after_attempt, wait_exponential
 from lib.objects_to_drive import ObjectsToDrive
 from typing import Any
 
@@ -15,6 +16,7 @@ class ObjectRetriever:
   def __init__(self, config) -> None:
     self.config = config
 
+  @retry(stop=stop_after_attempt(4), wait=wait_exponential(multiplier=1, min=2, max=120))
   def flush(self, obj, filename) -> None:
     if not os.path.exists(OUTPUT_FOLDER):
       os.mkdir(OUTPUT_FOLDER)
@@ -26,6 +28,7 @@ class ObjectRetriever:
     objects_to_drive = ObjectsToDrive()
     objects_to_drive.save(self.config, filename, local_file)
 
+  @retry(stop=stop_after_attempt(4), wait=wait_exponential(multiplier=1, min=2, max=120))
   def load(self, filename) -> Any:
     objects_to_drive = ObjectsToDrive()
     from_drive = objects_to_drive.load(self.config, filename)
