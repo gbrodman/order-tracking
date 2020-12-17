71e1598b1d1da12e5f532cd6e692e5744b15be52
jmizrahi <joseph.mizrahi@disneystreaming.com>
2020-12-16 22:23:11 -0500

    Fix config

--- lib/email_tracking_retriever.py
+++ lib/email_tracking_retriever.py
@@ -74,7 +74,7 @@ class EmailTrackingRetriever(ABC):
 
     self.driver = self.driver_creator.new()
     try:
-      log_in_if_necessary(self.driver)
+      log_in_if_necessary(self.driver, self.amazon_config)
       for email_id in tqdm(self.all_email_ids, desc="Fetching trackings", unit="email"):
         try:
           for attempt in range(MAX_ATTEMPTS):
@@ -289,20 +289,20 @@ def clean_email_content(email_str) -> str:
   return email_str
 
 
-def log_in_if_necessary(driver: WebDriver):
+def log_in_if_necessary(driver: WebDriver, amazon_config):
   driver.get('https://www.amazon.com/gp/your-account/order-history/ref=ppx_yo_dt_b_orders')
   orders_containers = driver.find_elements_by_id('ordersContainer')
   if len(orders_containers) == 0:
     # likely a login screen
-    if "email" in AMAZON and AMAZON["email"]:
+    if "email" in amazon_config and amazon_config["email"]:
       print("Signing into Amazon ...")
       driver.find_element_by_css_selector('form[name="signIn"]')
-      driver.find_element_by_css_selector('input[type="email"]').send_keys(AMAZON['email'])
+      driver.find_element_by_css_selector('input[type="email"]').send_keys(amazon_config['email'])
       driver.find_element_by_css_selector('input[type="submit"]').click()
-      driver.find_element_by_css_selector('input[type="password"]').send_keys(AMAZON['password'])
+      driver.find_element_by_css_selector('input[type="password"]').send_keys(amazon_config['password'])
       driver.find_element_by_css_selector('input[type="submit"]').click()
 
-      if (AMAZON['twoFactor']):
+      if (amazon_config['twoFactor']):
         input('Enter your OTP on the opened Chrome profile. Hit ENTER when done.')
     else:
       input('Please log in to an Amazon account on the opened Chrome profile. Hit ENTER when done.')
