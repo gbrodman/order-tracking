f536f34965cf8bca78ab51f187177f87a35e7e5e
Gustav Brodman <gus.brodman@gmail.com>
2021-01-21 09:31:09 -0500

    Update BFMR site

--- lib/group_site_manager.py
+++ lib/group_site_manager.py
@@ -477,15 +477,15 @@ class GroupSiteManager:
     try:
       # load the login page first
       self._load_page(driver, "https://buyformeretail.com/login")
-      driver.find_element_by_id("loginEmail").send_keys(group_config['username'])
-      driver.find_element_by_id("loginPassword").send_keys(group_config['password'])
-      driver.find_element_by_xpath("//button[@type='submit']").click()
+      driver.find_element_by_id("email").send_keys(group_config['username'])
+      driver.find_element_by_id("password").send_keys(group_config['password'])
+      driver.find_element_by_css_selector('div.bfmr-auth-form-next button').click()
 
       time.sleep(2)
 
       # hope there's a button to submit tracking numbers -- it doesn't matter which one
       try:
-        driver.find_element_by_class_name('btn-bf-submit-track').click()
+        driver.find_element_by_css_selector('div.view-deal-section button').click()
       except NoSuchElementException:
         raise Exception(
             "Could not find submit-trackings button. Make sure that you've subscribed to a deal and that the login credentials are correct"
@@ -495,7 +495,7 @@ class GroupSiteManager:
 
       textarea = driver.find_element_by_css_selector('div.modal-content textarea')
       textarea.send_keys("\n".join(numbers))
-      driver.find_element_by_css_selector('div.modal-content button.btn-primary').click()
+      driver.find_element_by_css_selector('div.modal-content button.btn-green').click()
       # TODO: This needs to wait for the success dialog to be displayed.
       time.sleep(5)
 
