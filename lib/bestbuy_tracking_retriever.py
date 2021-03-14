ed38cd90be9083de9f863f6c45667edcc51da372
Gustav Brodman <gus.brodman@gmail.com>
2021-03-14 13:22:04 -0700

    Attempt to capture multiple bby trackings

--- lib/bestbuy_tracking_retriever.py
+++ lib/bestbuy_tracking_retriever.py
@@ -9,7 +9,9 @@ from lib.email_tracking_retriever import EmailTrackingRetriever, AddressStrAndTr
 class BestBuyTrackingRetriever(EmailTrackingRetriever):
 
   tracking_regexes = [
+      r'[^0-9A-Z](1Z[0-9A-Z]{16})[^0-9A-Z]',
       r'Tracking #[<>br \/]*<a href="[^>]*>([A-Za-z0-9.]+)<\/a>',
+      r'>Tracking #: ([0-9A-Z]+)<',
       r'color:[^>]+>([0-9][0-9A-Z]+)<\/a>'
   ]
   order_id_regex = r'(BBY(?:01|TX)-\d{12})'
@@ -26,14 +28,15 @@ class BestBuyTrackingRetriever(EmailTrackingRetriever):
   def get_price_from_email(self, raw_email):
     return None  # not implementable
 
-  def get_tracking_numbers_from_email(self, email_str) -> List[Tuple[str, Optional[str]]]:
+  def get_tracking_numbers_from_email(self, raw_email, from_email: str,
+                                      to_email: str) -> List[Tuple[str, Optional[str]]]:
+    all_trackings = set()
     for regex in self.tracking_regexes:
-      match = re.search(regex, email_str)
-      if match:
-        # The second part of the tuple here is the shipping status, which would need
-        # to be retrieved from a shipping status web page (like it is for Amazon).
-        return [(match.group(1), None)]
-    return []
+      for match in re.finditer(regex, raw_email):
+        all_trackings.add(match.group(1))
+    # The second part of the tuple here is the shipping status, which would need
+    # to be retrieved from a shipping status web page (like it is for Amazon).
+    return [(t, None) for t in all_trackings]
 
   def get_subject_searches(self):
     return [["Your order #BBY01", "has shipped"], ["Your order #BBYTX", "has shipped"]]
