da8711b78609afbe6ea509dfb43c7c25bdd492f6
Gustav Brodman <gus.brodman@gmail.com>
2021-08-25 17:17:33 -0400

    oops

--- import_report.py
+++ import_report.py
@@ -185,7 +185,7 @@ def download_personal_report(driver: WebDriver) -> None:
   driver.get(PERSONAL_REPORT_URL)
   type_select = Select(driver.find_element_by_id('report-type'))
   type_select.select_by_visible_text('Orders and shipments')
-  driver.execute_script('setDatesToYearToLastThirtyDays()')
+  driver.execute_script('setDatesToLastThirtyDays()')
   prime_promo_elems = do_with_wait(
       driver, 0.5, 10, lambda: driver.find_elements_by_css_selector('div.nav-prime-tt.nav-flyout'))
   if prime_promo_elems:
